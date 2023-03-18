32ede0a9de3bf38056e18cc2dcd89d7f273d4462
Riyaz Usmanov <usmanov.riyaz22@gmail.com>
2023-03-18 23:03:57 +0300

    ex03

---
+++ ex02/PmergeMe.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include <deque>
+#include <list>
+#include <algorithm>
+#include <ctime>
+#include <cstdlib>
+#include <iterator>
+
+class PmergeMe
+{
+	public:
+	PmergeMe(int ac, char **av);
+	template <typename T>
+	void display(const T& container);
+	void mergeInsertSortDeque(std::deque<int>& arr);
+	void mergeInsertSortList(std::list<int>& arr);
+};
